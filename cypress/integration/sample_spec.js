5690b9cc5324d844fde5ea0881d2a3a7431148ad
Jay Papisan <jpapisan@gmail.com>
2020-12-03 14:32:29 -0800

    Adds cypress tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,24 +1,41 @@
 describe('Testing Cypress', () => {
-    it('finds the content "type"', () => {
-      cy.visit('/')
-  
-      cy.contains('nuxt-test')
+  // 1
+  it('Test homepage URL', () => {
+    cy.visit('/')
+    cy.location().should((loc) => {
+      expect(loc.host).to.eq('localhost:3000')
     })
   })
-
-  describe('Testing Name and Age calculator', () => {
-    it('finds the content "type"', () => {
-      cy.visit('/age')
-  
-      cy.contains('nuxt-test')
-    })
+  // 2
+  it('finds the content "type"', () => {
+    cy.visit('/')
+    cy.get('h1').should('contain', 'nuxt-test')
   })
+})
 
-
-  describe('Testing Color input', () => {
-    it('finds the content "type"', () => {
-      cy.visit('/events')
-  
-      cy.contains('nuxt-test')
-    })
+describe('Testing Name and Age calculator', () => {
+  // 3
+  it('Test Age calculator', () => {
+    cy.visit('/age')
+    cy.get('input[placeholder="Add Age"]')
+      .click()
+      .type('100')
+      .wait(100)
+      .get('#age-text')
+      .contains('105')
   })
+})
+
+describe('Testing Color input', () => {
+  // 4
+  it('Color input displays', () => {
+    cy.visit('/events')
+    cy.get('input[placeholder="enter favorite color"]')
+      .click()
+      .type('purple')
+      .type('{enter}')
+      .wait(100)
+      .get('h1')
+      .contains('purple')
+  })
+})
--- e2e/app.test.js
+++ e2e/app.test.js
@@ -6,23 +6,21 @@ const origin = 'http://localhost:3000/'
 
 fixture `Testing testcafe`
     .page `${origin}`
-
+ // 1
 test('Test homepage URL', async t => {
-    // assertion to check if url is what it should be
     await t.expect(origin).match(/http:\/\/localhost:3000/);
 });
-
+// 2
 test('Check title text', async (t) => {
   const title = Selector('h1').innerText
-  // assertion to check if the only h1 tag on the page contains the required text
   await t.expect(title).contains('nuxt-test');
 });
 
 fixture `Testing Name and Age calculator`
     .page `${origin}age`
-
+// 3
 test('Test Age calculator', async t => {
-    const ageInput = Selector('input').withAttribute('placeholder', 'Add Age')
+    const ageInput = Selector('input[placeholder="Add Age"]')
     await t
     .click(ageInput)
     .typeText(ageInput, '100', { replace: true })
@@ -32,7 +30,7 @@ test('Test Age calculator', async t => {
 
 fixture `Testing Color input`
     .page `${origin}events`
-
+// 4
 test('Color input displays', async t => {
     const colorInput = Selector('input').withAttribute('placeholder', 'enter favorite color')
     await t
