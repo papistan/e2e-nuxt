f59da7181f737e65ab45df630fb919af51767c04
Jay Papisan <jpapisan@gmail.com>
2020-12-03 15:54:29 -0800

    Adds webpackmonitor

---
+++ e2e/a11y.test.js
@@ -0,0 +1,11 @@
+// import { axeCheck, createReport } from "axe-testcafe";
+
+// const origin = 'http://localhost:3000/'
+
+// fixture `Homepage a11y`
+//     .page `${origin}`
+
+// test("Accessibility testing", async t => {
+//   const { error, violations } = await axeCheck(t);
+//   await t.expect(violations.length === 0).ok(createReport(violations));
+// });
